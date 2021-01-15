a9b722a2e3cc4e42a1e3c62bf793f2a1e91dd739
Kishan Jadav <kishan_jadav@hotmail.com>
2021-01-15 23:20:28 +0000

    added a generic piece, with movement across the table grid, and placement.. still loads to do in here..

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import { Stage, Layer } from 'react-konva';
 import BoardLayer from './components/BoardLayer';
+import Piece from './components/Piece';
 
 
 const boardSize = 700;
 
 function App() {
-  return (
-    <Stage width={boardSize} height={boardSize}>
-        <BoardLayer boardSize={boardSize} />
-    </Stage>
-  );
+	return (
+		<Stage width={boardSize} height={boardSize}>
+			<BoardLayer boardSize={boardSize} />
+
+			<Layer>
+                
+				{
+					[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(col => (
+						<Piece
+							col={col}
+							row={7}
+							color="blue"
+							gridSize={boardSize / 10} />
+					))
+				}
+
+			</Layer>
+		</Stage>
+	);
 }
 
 export default App;
