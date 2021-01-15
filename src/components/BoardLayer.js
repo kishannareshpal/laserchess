1b51d24ef636623f6ba46b0bc68f38650b9d63cb
Kishan Jadav <kishan_jadav@hotmail.com>
2021-01-15 23:19:04 +0000

    eslint fix and added keys for the loop

--- src/components/BoardLayer.js
+++ src/components/BoardLayer.js
@@ -8,72 +8,72 @@ import WhiteLazerSlotSVG from '../assets/white-lazer-slot.svg';
 import useImage from 'use-image';
 
 const arrangement = {
-    columns: 10,
-    rows: 8,
-    slots: [
-        ["white-lazer", "white-helix", "normal", "normal", "normal", "normal", "normal", "normal", "red-helix", "white-helix"],
-        ["red-helix", "normal", "normal", "normal", "normal", "normal", "normal", "normal", "normal", "white-helix"],
-        ["red-helix", "normal", "normal", "normal", "normal", "normal", "normal", "normal", "normal", "white-helix"],
-        ["red-helix", "normal", "normal", "normal", "normal", "normal", "normal", "normal", "normal", "white-helix"],
-        ["red-helix", "normal", "normal", "normal", "normal", "normal", "normal", "normal", "normal", "white-helix"],
-        ["red-helix", "normal", "normal", "normal", "normal", "normal", "normal", "normal", "normal", "white-helix"],
-        ["red-helix", "normal", "normal", "normal", "normal", "normal", "normal", "normal", "normal", "white-helix"],
-        ["red-helix", "white-helix", "normal", "normal", "normal", "normal", "normal", "normal", "red-helix", "red-lazer"]
-    ]
-}
+	columns: 10,
+	rows: 8,
+	slots: [
+		['white-lazer', 'white-helix', 'normal', 'normal', 'normal', 'normal', 'normal', 'normal', 'red-helix', 'white-helix'],
+		['red-helix', 'normal', 'normal', 'normal', 'normal', 'normal', 'normal', 'normal', 'normal', 'white-helix'],
+		['red-helix', 'normal', 'normal', 'normal', 'normal', 'normal', 'normal', 'normal', 'normal', 'white-helix'],
+		['red-helix', 'normal', 'normal', 'normal', 'normal', 'normal', 'normal', 'normal', 'normal', 'white-helix'],
+		['red-helix', 'normal', 'normal', 'normal', 'normal', 'normal', 'normal', 'normal', 'normal', 'white-helix'],
+		['red-helix', 'normal', 'normal', 'normal', 'normal', 'normal', 'normal', 'normal', 'normal', 'white-helix'],
+		['red-helix', 'normal', 'normal', 'normal', 'normal', 'normal', 'normal', 'normal', 'normal', 'white-helix'],
+		['red-helix', 'white-helix', 'normal', 'normal', 'normal', 'normal', 'normal', 'normal', 'red-helix', 'red-lazer']
+	]
+};
 
 const BoardLayer = ({ boardSize }) => {
-    const [slotSize] = useState(boardSize / arrangement.columns)
-    const [normalSlotImage] = useImage(NormalSlotSVG);
-    const [whiteLazerSlotImage] = useImage(WhiteLazerSlotSVG);
-    const [whiteHelixSlotImage] = useImage(WhiteHelixSlotSVG);
-    const [redLazerSlotImage] = useImage(RedLazerSlotSVG);
-    const [redHelixSlotImage] = useImage(RedHelixSlotSVG);
+	const [slotSize] = useState(boardSize / arrangement.columns);
+	const [normalSlotImage] = useImage(NormalSlotSVG);
+	const [whiteLazerSlotImage] = useImage(WhiteLazerSlotSVG);
+	const [whiteHelixSlotImage] = useImage(WhiteHelixSlotSVG);
+	const [redLazerSlotImage] = useImage(RedLazerSlotSVG);
+	const [redHelixSlotImage] = useImage(RedHelixSlotSVG);
 
 
-    const getSlotImage = useCallback((slotType) => {
-        switch (slotType) {
-            case "white-helix":
-                return whiteHelixSlotImage;
+	const getSlotImage = useCallback((slotType) => {
+		switch (slotType) {
+		case 'white-helix':
+			return whiteHelixSlotImage;
 
-            case "red-helix":
-                return redHelixSlotImage;
+		case 'red-helix':
+			return redHelixSlotImage;
         
-            case "white-lazer":
-                return whiteLazerSlotImage;
+		case 'white-lazer':
+			return whiteLazerSlotImage;
             
-            case "red-lazer":
-                return redLazerSlotImage;
+		case 'red-lazer':
+			return redLazerSlotImage;
 
-            default:
-                return normalSlotImage;
+		default:
+			return normalSlotImage;
 
-        }
-    }, [whiteHelixSlotImage, redHelixSlotImage, whiteLazerSlotImage, redLazerSlotImage, normalSlotImage])
+		}
+	}, [whiteHelixSlotImage, redHelixSlotImage, whiteLazerSlotImage, redLazerSlotImage, normalSlotImage]);
 
 
-    const drawSlotsGrid = useCallback(() => {
-        const rows = []
-        arrangement.slots.forEach((row, rowIndex) => {
-            row.forEach((col, colIndex) => {
-                rows.push(
-                    <Image image={getSlotImage(col)} 
-                        x={0 + (slotSize * colIndex)}
-                        y={0 + (slotSize * rowIndex)} 
-                        width={slotSize} 
-                        height={slotSize} />
-                )
-            })
-        })
-        return rows;
-    }, [slotSize, getSlotImage])
+	const drawSlotsGrid = useCallback(() => {
+		const rows = [];
+		arrangement.slots.forEach((row, rowIndex) => {
+			row.forEach((col, colIndex) => {
+				rows.push(
+					<Image key={`${rowIndex}${colIndex}`} image={getSlotImage(col)} 
+						x={0 + (slotSize * colIndex)}
+						y={0 + (slotSize * rowIndex)} 
+						width={slotSize} 
+						height={slotSize} />
+				);
+			});
+		});
+		return rows;
+	}, [slotSize, getSlotImage]);
 
     
-    return (
-        <Layer>
-            { drawSlotsGrid() }
-        </Layer>
-    )
-}
+	return (
+		<Layer>
+			{ drawSlotsGrid() }
+		</Layer>
+	);
+};
 
-export default BoardLayer
+export default BoardLayer;
