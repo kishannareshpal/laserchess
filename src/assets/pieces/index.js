1c313bb8aa63ebb1c739818a60e937f6ed893a50
Kishan Jadav <kishan_jadav@hotmail.com>
2021-01-25 23:27:54 +0000

    improved assets design

---
+++ src/assets/pieces/index.js
@@ -0,0 +1,23 @@
+const BlueKingPieceSVG      = require('./blue-king.svg').default;
+const RedKingPieceSVG       = require('./red-king.svg').default;
+const BlueLaserPieceSVG     = require('./blue-laser.svg').default;
+const RedLaserPieceSVG      = require('./red-laser.svg').default;
+const BlueSwitchPieceSVG    = require('./blue-switch.svg').default;
+const RedSwitchPieceSVG     = require('./red-switch.svg').default;
+const BlueDefenderPieceSVG  = require('./blue-defender.svg').default;
+const RedDefenderPieceSVG   = require('./red-defender.svg').default;
+const BlueDeflectorPieceSVG = require('./blue-deflector.svg').default;
+const RedDeflectorPieceSVG  = require('./red-deflector.svg').default;
+
+export {
+    BlueKingPieceSVG, 
+    RedKingPieceSVG,
+    BlueLaserPieceSVG, 
+    RedLaserPieceSVG, 
+    BlueSwitchPieceSVG, 
+    RedSwitchPieceSVG, 
+    BlueDefenderPieceSVG, 
+    RedDefenderPieceSVG, 
+    BlueDeflectorPieceSVG, 
+    RedDeflectorPieceSVG
+};
